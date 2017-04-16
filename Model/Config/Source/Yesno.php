85adeba0029725aacc9887b0e40a3a17087f4c00
Amit <amit103065@gmail.com>
2017-04-16 17:00:23 +0530

    add customer account approval config section

---
+++ Model/Config/Source/Yesno.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Used in creating options for Yes|No config value selection
+ *
+ */
+namespace Amitshree\Marketplace\Model\Config\Source;
+
+class Yesno implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [['value' => 1, 'label' => __('Yes')], ['value' => 0, 'label' => __('No')]];
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return [0 => __('No'), 1 => __('Yes')];
+    }
+}
