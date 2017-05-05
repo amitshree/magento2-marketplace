47f1f7b70995e463ffa9df98b3f2bc8ab834193b
Amit <amit103065@gmail.com>
2017-05-05 17:58:37 +0530

    test module in real and test environment

--- Test/Integration/ModuleConfigTest.php
+++ Test/Integration/ModuleConfigTest.php
@@ -20,10 +20,48 @@ class ModuleConfigTest extends \PHPUnit_Framework_TestCase
         $this->subjectModuleName = 'Amitshree_Marketplace';
         $this->objectManager = ObjectManager::getInstance();
     }
+
     public function testTheModuleIsRegistered()
     {
         $registrar = new ComponentRegistrar();
         $this->assertArrayHasKey('Amitshree_Marketplace', $registrar->getPaths(ComponentRegistrar::MODULE));
     }
 
+    public function testTheModuleIsConfiguredInTheTestEnvironment()
+    {
+        /**
+         * @var $moduleList ModuleList
+         */
+        $moduleList = $this->objectManager->create(ModuleList::class);
+        $this->assertTrue($moduleList->has($this->subjectModuleName));
+    }
+
+    public function testTheModuleIsConfiguredInRealEnvironment()
+    {
+        /**
+         * @var $objectManager ObjectManager
+         */
+        $this->objectManager = ObjectManager::getInstance();
+
+        // The tests by default point to the wrong config directory for this test.
+        $directoryList = $this->objectManager->create(
+            \Magento\Framework\App\Filesystem\DirectoryList::class,
+            ['root' => BP]
+        );
+        $deploymentConfigReader = $this->objectManager->create(
+            \Magento\Framework\App\DeploymentConfig\Reader::class,
+            ['dirList' => $directoryList]
+        );
+        $deploymentConfig = $this->objectManager->create(
+            \Magento\Framework\App\DeploymentConfig::class,
+            ['reader' => $deploymentConfigReader]
+        );
+
+        /** @var $moduleList ModuleList */
+        $moduleList = $this->objectManager->create(
+            ModuleList::class,
+            ['config' => $deploymentConfig]
+        );
+        $this->assertTrue($moduleList->has($this->subjectModuleName));
+    }
 }
